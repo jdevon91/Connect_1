3e1abc0224b3cbee816afe2a4156bbfb64997298
jdevon91 <jbudhlall@gmail.com>
2017-02-08 16:17:33 -0700

    Login UI added.

---
+++ Connect_1/SignInVC.swift
@@ -0,0 +1,18 @@
+//
+//  SignInVC.swift
+//  Connect_1
+//
+//  Created by Jesse Budhlall on 2017-02-08.
+//  Copyright © 2017 Jesse Budhlall. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
