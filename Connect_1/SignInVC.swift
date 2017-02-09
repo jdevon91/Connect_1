39b859dc4c20199c00154f17f934478ea525e7ed
jdevon91 <jbudhlall@gmail.com>
2017-02-08 23:34:28 -0700

    E-mail authentication implemented.

--- Connect_1/SignInVC.swift
+++ Connect_1/SignInVC.swift
@@ -7,8 +7,33 @@
 //
 
 import UIKit
+import Firebase
 
 class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBAction func signInBtn(_ sender: Any) {
+        
+        if let email = emailField.text, let pwd = passwordField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pwd, completion: { (user, error) in
+                if error == nil {
+                    print("Jesse: email user authenticated w/Firebase")
+                }
+                else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
+                        if error != nil {
+                            print("Jesse: email user not authenticated w/Firebase ")
+                        }
+                        else{
+                            print("Jesse: email user authenticated w/Firebase")
+                        }
+                    })
+                }
+            })
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
